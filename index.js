2d1f62e167bc06f880a5a1833936547bbb59f22d
Dale Dahlenburg <dale.19@icloud.com>
2023-01-18 15:09:54 +1000

    basic route added

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 4001;
+
+// Enable CORS
+app.use(cors());
+
+// Parse incoming JSON data
+app.use(express.json());
+
+// Root route
+app.get("/", (requ, res) => res.send({ info: "Dinner Mate" }));
+
 
 app.listen(port, () => console.log('App running at http://localhost:${port}/'));
 
