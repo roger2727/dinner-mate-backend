ea689632e10e79820328369975316247a521bf15
Dale Dahlenburg <dale.19@icloud.com>
2023-01-18 15:06:03 +1000

    Running at port 4001

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+import express from "express";
+
+const app = express();
+const port = 4001;
+
+app.listen(port, () => console.log('App running at http://localhost:${port}/'));
+
+
