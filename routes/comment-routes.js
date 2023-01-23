25a55558ee5e3df23c3b91591e94059c527e11c4
Dale Dahlenburg <dale.19@icloud.com>
2023-01-23 13:48:40 +1000

    fixed comments

--- routes/comment-routes.js
+++ routes/comment-routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { RecipeModel } from "../models/recipe.js";
 import authenticateJWT from "../middleware/jwt-auth.js";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 import { CommentModel } from "../models/comment.js";
 
@@ -10,14 +11,14 @@ dotenv.config();
 const router = express.Router();
 
 // Get all comments for recipe
-router.get('/comments/:recipe', async (req, res) => {
+router.get('/:recipeId', async (req, res) => {
     try {
-        // const title = JSON.parse(req.query.title);
-
-        const comments = await CommentModel.find({ 
-            recipe: req.params.recipe
-        })
-        
+        // const isValid = mongoose.Types.ObjectId.isValid(req.params.recipeId)
+        const recipeId = req.params.recipeId;
+        const recipe = await RecipeModel.findById(recipeId)
+        const comments = await CommentModel.find({
+            recipe: recipe
+        })        
         res.json({ comments })
     } catch (err) {
         console.log("error", err);
@@ -26,17 +27,18 @@ router.get('/comments/:recipe', async (req, res) => {
 });
 
 // Create new comment for recipe
-router.post('/:recipe/add', authenticateJWT, async (req, res) => {
+router.post('/add/:recipeId', authenticateJWT, async (req, res) => {
     try {
         const { title, commentText, userRating } = req.body;
         const { userId } = req.user;
-        const recipeId = await RecipeModel.find({ recipeId: req.params.recipeId });
+        const recipeId = req.params.recipeId;
+        const recipe = await RecipeModel.findById(recipeId)
         const comment = new CommentModel({
             title,
             commentText,
             userRating,
             user: userId,
-            recipe: recipeId
+            recipe: recipe
         });
         await comment.save();
         res.send({ comment });
