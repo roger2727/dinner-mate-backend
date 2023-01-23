a9a1b3fd8e145594af84c2d78872b9faf110c5fd
Dale Dahlenburg <dale.19@icloud.com>
2023-01-23 12:28:17 +1000

    adding comment routes

---
+++ routes/comment-routes.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { RecipeModel } from "../models/recipe.js";
+import authenticateJWT from "../middleware/jwt-auth.js";
+import dotenv from "dotenv";
+
+import { CommentModel } from "../models/comment.js";
+
+dotenv.config();
+
+const router = express.Router();
+
+// Get all comments for recipe
+router.get('/:recipeId/comments', async (req, res) => {
+    try {
+        const comments = await CommentModel.find({ 
+            recipeId: req.params.recipeId
+        })
+
+        res.json({ comments })
+    } catch (err) {
+        console.log("error", err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Create new comment for recipe
+router.post('/:recipeId/add', authenticateJWT, async (req, res) => {
+    try {
+        const { title, commentText, userRating } = req.body;
+        const { userId } = req.user;
+        const recipeId = await RecipeModel.find({ recipeId: req.params.recipeId });
+        const comment = new CommentModel({
+            title,
+            commentText,
+            userRating,
+            user: userId,
+            recipe: recipeId
+        });
+        await comment.save();
+        res.send({ comment });
+    } catch (err) {
+        res.status(500).send({ error: error.message });
+    }
+});
