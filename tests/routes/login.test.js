82696263873e0f018ac3fac389d1bcdeb68526be
Mitchell Roger <roger.mitch27@gmail.com>
2023-01-24 23:33:36 +1100

    fixed comment msg

--- tests/routes/login.test.js
+++ tests/routes/login.test.js
@@ -1,5 +1,3 @@
-// This test is checking that when a user sends a POST request to the "/auth/login" route with a valid email and password in the request body, the server should return a JSON response with a token and a message indicating a successful login. The test creates a new user with a hashed password in the test database before each test, and then sends a request with the email and plaintext password to the route. The test then asserts that the response has a "token" property and a "msg" property with the value "You have successfully logged in". The test also closes the server connection after all tests are run.
-
 import request from "supertest";
 import app from "../../app"; // import your express app
 import { UserModel } from "../../models/user";
